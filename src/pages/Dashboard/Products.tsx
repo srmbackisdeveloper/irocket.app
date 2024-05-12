663b6c4fa5c016681fec1c8c68ed7358e1625b1c
sarsembek <ar_sarsembek@kbtu.kz>
2024-05-12 15:05:31 +0500

    fix

--- src/pages/Dashboard/Products.tsx
+++ src/pages/Dashboard/Products.tsx
@@ -1,6 +1,3 @@
-import { DashboardFooter } from "../../components/shared/DashboardFooter"
-import DashboardHeader from "../../components/shared/DashboardHeader"
-
 export const Products = () => {
     return(
         <div>
--- src/pages/Dashboard/Shops.tsx
+++ src/pages/Dashboard/Shops.tsx
@@ -1,6 +1,3 @@
-import { DashboardFooter } from "../../components/shared/DashboardFooter"
-import DashboardHeader from "../../components/shared/DashboardHeader"
-
 export const Shops = () => {
     return(
         <div>
