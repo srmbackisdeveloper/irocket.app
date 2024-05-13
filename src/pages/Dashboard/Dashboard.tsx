4a05056aed3e0127c82c79fda3b52e06d7c09a4d
szholbarys <sakenzholbarys@gmail.com>
2024-05-14 03:22:10 +0500

    dashboard footer sticked

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,15 +1,17 @@
-import DashboardHeader from '../../components/shared/DashboardHeader'
-import { DashboardFooter } from '../../components/shared/DashboardFooter'
-import { Outlet } from 'react-router-dom'
+import DashboardHeader from "../../components/shared/DashboardHeader"
+import { DashboardFooter } from "../../components/shared/DashboardFooter";
+import { Outlet } from "react-router-dom";
 
 const Dashboard = () => {
-   return (
-      <>
-         <DashboardHeader />
-         <Outlet />
-         <DashboardFooter />
-      </>
-   )
-}
+  return (
+    <div className="flex flex-col min-h-screen">
+      <DashboardHeader />
+      <div className="flex-grow">
+        <Outlet />
+      </div>
+      <DashboardFooter />
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
