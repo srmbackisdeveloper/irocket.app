054e4c536ddfc3ab1bbcf19429f86d471c56205e
sarsembek <ar_sarsembek@kbtu.kz>
2024-05-15 05:03:58 +0500

    hotfix

--- src/components/Dashboard/Products/ProductSearch.tsx
+++ src/components/Dashboard/Products/ProductSearch.tsx
@@ -1,4 +1,4 @@
-import { Select, SelectItem } from "@nextui-org/react";
+import { Button, Select, SelectItem } from "@nextui-org/react";
 import { Search } from "../../shared/icons/Search.icon";
 import { useState } from "react";
 
@@ -20,18 +20,18 @@ export const ProductSearch = () => {
 
     return (
         <div className="">
-            <div className="grid border rounded-lg p-3">
+            <div className="flex items-center justify-between border rounded-lg p-3">
                 <div className="flex items-center gap-3">
-                <div className="flex border-gray-200 border-2 rounded-lg p-[6px] gap-2 max-w-[16em] max-h-fit items-center focus:border-black duration-200 focus-within:border-black">
-                    <Search/>
-                    <input type="text" placeholder="Артикул / Название" className=""/>
-                </div>
+                    <div className="flex border-gray-200 border-2 rounded-lg p-[6px] gap-2 max-w-[16em] max-h-fit items-center focus:border-black duration-200 focus-within:border-black">
+                        <Search/>
+                        <input type="text" placeholder="Артикул / Название" className=""/>
+                    </div>
                     <Select
                         variant="bordered"
                         radius="sm"
                         value={selectedSize}
                         defaultSelectedKeys={[selectedSize]}
-                        className="max-w-[4.5rem]"
+                        className="min-w-[4.5rem]"
                     >
                         {labeledData.map((data) => (
                             <SelectItem key={data.value} value={data.value} onChange={() => handleSizeChange(data.value)}>
@@ -40,7 +40,13 @@ export const ProductSearch = () => {
                         ))}
                     </Select>
                 </div>
-
+                <Button
+                   color="danger"
+                   variant="bordered"
+                   className="text-black px-7 font-semibold hover:bg-danger hover:text-white"
+                >
+                   Обновить
+                </Button>
             </div>
             <div>
 
