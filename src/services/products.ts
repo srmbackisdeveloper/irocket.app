1ea7c73f9c0ff7854a90d4e15ca2ff2a4a6bdd11
szholbarys <sakenzholbarys@gmail.com>
2024-05-20 23:11:59 +0500

    fix conflict

--- src/services/products.ts
+++ src/services/products.ts
@@ -3,7 +3,7 @@ import { axiosInstance } from './api';
 
 class ProductsAPI {
     private axios = axiosInstance("kaspi_products");
-
+    
     getProducts = async (): Promise<TProducts[]> => {
         const response = await this.axios.get<TProducts[]>("/");
         return Array.isArray(response.data) ? response.data : [];
