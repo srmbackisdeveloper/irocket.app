03ffe6adca8c09843f2e882bac46eec7d71ad9ae
sarsembek <ar_sarsembek@kbtu.kz>
2024-05-23 20:44:25 +0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import { VitePWA } from 'vite-plugin-pwa';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
