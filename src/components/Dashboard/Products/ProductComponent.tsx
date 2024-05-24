230936a3724c2a21e98e58043fb84bca3711fc9e
szholbarys <sakenzholbarys@gmail.com>
2024-05-25 04:51:40 +0500

    added new pagination

--- src/components/Dashboard/Products/ProductComponent.tsx
+++ src/components/Dashboard/Products/ProductComponent.tsx
@@ -1,7 +1,7 @@
+import React, { useState } from "react";
 import { ProductList } from "./ProductList";
 import { useGetProducts } from "../../../hooks/useGetProducts";
-import { Tooltip } from "@nextui-org/react";
-import { useState } from "react";
+import { Tooltip, Pagination } from "@nextui-org/react";
 
 export const ProductComponent = () => {
   const [page, setPage] = useState(1);
@@ -10,12 +10,8 @@ export const ProductComponent = () => {
 
   const totalPages = Math.ceil((query.data?.count ?? 0) / limit);
 
-  const handlePreviousPage = () => {
-    setPage((prev) => Math.max(prev - 1, 1));
-  };
-
-  const handleNextPage = () => {
-    setPage((prev) => (prev < totalPages ? prev + 1 : prev));
+  const handlePageChange = (page: number) => {
+    setPage(page);
   };
 
   const hasProducts = (query.data?.results ?? []).length > 0;
@@ -49,29 +45,15 @@ export const ProductComponent = () => {
           <ProductList query={query} />
         </tbody>
       </table>
-      <div className="flex justify-between items-center p-4 mt-[2em]">
-        <button
-          className="px-4 py-2 bg-danger text-white rounded-md"
-          onClick={handlePreviousPage}
-          disabled={page === 1}
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M21 16.811c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061A1.125 1.125 0 0 1 21 8.689v8.122ZM11.25 16.811c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061a1.125 1.125 0 0 1 1.683.977v8.122Z" />
-          </svg>
-        </button>
-        <span>
-          Страница {page} из {totalPages}
-        </span>
-        <button
-          className="px-4 py-2 bg-danger text-white rounded-md"
-          onClick={handleNextPage}
-          disabled={page === totalPages}
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M3 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061A1.125 1.125 0 0 1 3 16.811V8.69ZM12.75 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061a1.125 1.125 0 0 1-1.683-.977V8.69Z" />
-          </svg>
-        </button>
+        <Pagination
+          className="grid justify-center items-center mt-[2em] px-[4em]"
+          showControls
+          color="danger"
+          size="lg"
+          total={totalPages}
+          initialPage={page}
+          onChange={(page: number) => handlePageChange(page)}
+        />
       </div>
-    </div>
   );
 };
