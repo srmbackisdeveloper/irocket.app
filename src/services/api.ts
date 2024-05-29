76cc22e1ec77c747a1d1524385c18fdfc0bcb967
sarsembek <ar_sarsembek@kbtu.kz>
2024-05-29 14:24:10 +0500

    fix axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,15 +2,29 @@ import Axios from "axios";
 
 export const axiosInstance = (url: string) => {
   const BASE_URL = "https://irocket.sky-ddns.kz";
-  const token = localStorage.getItem('token');
 
+  // Create an Axios instance
   const axios = Axios.create({
     baseURL: `${BASE_URL}/${url}`,
     timeout: 5000,
     headers: {
       "Content-Type": "application/json",
-      ...(token && { "Authorization": `Token ${token}` })
     },
   });
+
+  // Add a request interceptor to include the token in each request
+  axios.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem('token');
+      if (token) {
+        config.headers.Authorization = `Token ${token}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
   return axios;
 };
